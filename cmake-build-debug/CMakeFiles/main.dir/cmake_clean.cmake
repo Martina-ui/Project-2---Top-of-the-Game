8966d94a46e14edb7a92946e5ad53fb5421ea9cb
Yusra89hash <yusra9189@gmail.com>
2025-11-03 17:55:28 -0500

    should be the rest of the files

---
+++ cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/HeapSort.cpp.o"
+  "CMakeFiles/main.dir/HeapSort.cpp.o.d"
+  "CMakeFiles/main.dir/Menu.cpp.o"
+  "CMakeFiles/main.dir/Menu.cpp.o.d"
+  "CMakeFiles/main.dir/MenuManager.cpp.o"
+  "CMakeFiles/main.dir/MenuManager.cpp.o.d"
+  "CMakeFiles/main.dir/heap_test.cpp.o"
+  "CMakeFiles/main.dir/heap_test.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "CMakeFiles/main.dir/mergeSort.cpp.o"
+  "CMakeFiles/main.dir/mergeSort.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
